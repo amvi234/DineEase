0ddaf8a01873bd45c2b850195ac1065715f5c1c4
Amit Vikram <amitv@aereo.io>
2024-06-03 18:22:48 +0530

    feat: updated Meta attribute for Receipe

--- base/vege/models.py
+++ base/vege/models.py
@@ -2,42 +2,53 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.base_user import AbstractBaseUser
 
+
 # Create your models here.
 class Receipe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null = True, blank = True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     receipe_name = models.CharField(max_length=100)
     receipe_description = models.TextField()
     receipe_price = models.IntegerField(default=0)
     receipe_view_count = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.receipe_name
+
 
 class CustomUser(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=50, unique=True)
 
+
 class Department(models.Model):
     department = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.department
-    
+
     class Meta:
-        ordering = ['department']
+        ordering = ["department"]
+
 
 class StudentId(models.Model):
     student_id = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.student_id
-    
+
+
 class Subject(models.Model):
     subject_name = models.CharField(max_length=100)
 
-    
-    
+
 class Student(models.Model):
-    department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
-    student_id = models.OneToOneField(StudentId, related_name="studentid", on_delete=models.CASCADE)
+    department = models.ForeignKey(
+        Department, related_name="depart", on_delete=models.CASCADE
+    )
+    student_id = models.OneToOneField(
+        StudentId, related_name="studentid", on_delete=models.CASCADE
+    )
     student_name = models.CharField(max_length=100)
     student_email = models.EmailField(unique=True)
 
@@ -45,16 +56,19 @@ class Student(models.Model):
         return self.student_name
 
     class Meta:
-        ordering = ['student_name']
+        ordering = ["student_name"]
         verbose_name = "student"
 
+
 class SubjectMarks(models.Model):
-    student = models.ForeignKey(Student, related_name="studentmarks", on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete = models.CASCADE)
+    student = models.ForeignKey(
+        Student, related_name="studentmarks", on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     marks = models.IntegerField()
 
     def __str__(self) -> str:
-        return f'{self.student.student_name} {self.subject.subject_name}'
+        return f"{self.student.student_name} {self.subject.subject_name}"
 
     class Meta:
-        unique_together = ['student', 'subject']
+        unique_together = ["student", "subject"]
